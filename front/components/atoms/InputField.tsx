9e5e104004bd71e8a088b870848e91de6a171480
Nikola <nikola.popovicc99@gmail.com>
2022-01-05 15:04:17 +0100

    refactor and enabled textfield styling

--- front/components/atoms/InputField.tsx
+++ front/components/atoms/InputField.tsx
@@ -1,16 +1,28 @@
-import { TextField, TextFieldProps } from "@mui/material";
-import { forwardRef } from "react";
+import { styled, TextField, TextFieldProps } from "@mui/material";
 
 type InputFieldProps = TextFieldProps & {
+    padding?: string
 }
 
-const InputField: React.FC<InputFieldProps> = forwardRef((props, ref) => {
-    return (
-        <TextField ref={ref} {...props}>
-            {props.children}
-        </TextField>
-    );
-});
+
+const InputField = styled(TextField, {
+    shouldForwardProp: prop => prop !== "textColor"
+})<InputFieldProps>(({ theme, padding }) => ({
+    backgroundColor: "white",
+    borderRadius: "0.25rem",
+    "& .MuiOutlinedInput-root": {  
+        padding,
+        "&:hover fieldset": {
+            borderColor: theme.color.primaryActive
+        },
+        '&.Mui-focused fieldset': { 
+            borderColor: theme.color.primaryHover,
+        }
+    },
+    label: {
+        padding
+    }
+}));
 
 InputField.defaultProps = {
     variant: "outlined"
