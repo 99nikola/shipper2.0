366888384c33fc1ae4b6bfb7df600472ccdf6179
Nikola <nikola.popovicc99@gmail.com>
2022-01-14 12:47:03 +0100

    special case for rendering items

--- front/components/templates/Slider.tsx
+++ front/components/templates/Slider.tsx
@@ -1,32 +1,32 @@
-import { ISliderItem } from "../../typescript";
+import { ISlider, ISliderItem } from "../../typescript";
 import SliderButtonLeft from "../molecules/SliderButtonLeft";
 import SliderButtonRight from "../molecules/SliderButtonRight";
-import SliderItems, { SliderItemsProps } from "../organisms/SliderItems";
+import SliderItems from "../organisms/SliderItems";
 import { ArrowLeft, ArrowRight } from "../styled/Arrows.syled";
 import Flex from "../styled/Flex.styled";
 
-interface SliderProps extends SliderItemsProps {
-    slideLeft: () => void,
-    slideRight: () => void
+interface SliderProps {
+    slider: ISlider,
+    render: (item: any) => React.ReactElement,
 }
-
 
 const Slider: React.FC<SliderProps> = (props) => {        
         return (
             <Flex> 
                 <SliderButtonLeft
-                    slide={props.slideLeft}
+                    slide={props.slider.left}
                     render={<ArrowLeft size="2.5rem" />}
                     />
 
                 <Flex direction="row-reverse">
                     <SliderButtonRight
-                        slide={props.slideRight}
+                        slide={props.slider.right}
                         render={<ArrowRight size="2.5rem" />}
                         />
                 <SliderItems 
-                    items={props.items}
+                    items={props.slider.items}
                     render={props.render}
+                    specialCase={props.slider.specialCase}
                 />
 
             </Flex>
