fbc062f6b9835983f78087030591bf424035069b
Nikola <nikola.popovicc99@gmail.com>
2022-01-14 15:27:45 +0100

    label added so it can be disalbed while click event is ongoing

--- front/utils/slide.ts
+++ front/utils/slide.ts
@@ -1,9 +1,13 @@
-export function delaySlide(checkbox: HTMLInputElement, slide: () => void) {
-    if (checkbox.checked)
+export function delaySlide(checkbox: HTMLInputElement, label: HTMLLabelElement, slide: () => void) {
+    const labelFor = label.htmlFor;
+    if (labelFor === "disabled")
         return;
+
+    label.htmlFor = "disabled";
 
     setTimeout(() => {
         slide();
+        label.htmlFor = labelFor;
         checkbox.checked = false;
     }, 250);
 }
