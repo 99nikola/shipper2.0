fa283b7b7f3e7a91e3a107bc40aeb4431d76fa7b
99nikola <nikola.popovicc99@gmail.com>
2022-01-16 16:57:20 +0100

    roboto font

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import RobotFont from '../components/layouts/RobotFont';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import Navbar from '../components/layouts/Navbar';
@@ -10,7 +9,6 @@ import { ThemeProvider } from '@emotion/react';
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	return (
 	<>
-		<RobotFont />
 		<Provider store={store}>
 			<ThemeProvider theme={theme}>
 				<Navbar>
