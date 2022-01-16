e23869e6e29cb1ec7f3932748e7b06a572d09674
99nikola <nikola.popovicc99@gmail.com>
2022-01-16 16:27:25 +0100

    SliderContainer ref added

--- front/components/organisms/SliderItems.tsx
+++ front/components/organisms/SliderItems.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import React, { forwardRef, useCallback, useEffect, useMemo, useRef } from "react";
 import { ISliderItem } from "../../typescript";
 import SliderContainer from "../styled/SliderContainer.styled";
 import SliderItem from "../styled/SliderItem.styled";
@@ -6,29 +6,48 @@ import SliderItem from "../styled/SliderItem.styled";
 interface SliderItemsProps {
     items: ISliderItem[],
     render: (item: ISliderItem) => React.ReactElement,
-    specialCase: boolean
+    specialCase: boolean,
+    setWidth: React.Dispatch<React.SetStateAction<any>>
 }
 
-const SliderItems: React.FC<SliderItemsProps> = (props) => {
+const SliderItems = forwardRef<HTMLDivElement, SliderItemsProps>((props, ref) => {
+
+    const itemRef = useRef<HTMLDivElement>(null);
+
+    const handleResize = useCallback(() => {
+        props.setWidth(itemRef.current?.offsetWidth);
+    }, [itemRef, props.setWidth]);
+
+    useEffect(() => {
+        if (itemRef.current === null)
+            return;
+
+        props.setWidth(itemRef.current.offsetWidth);
+        itemRef.current.addEventListener("resize", handleResize);
+
+        return () => itemRef.current?.removeEventListener("resize", handleResize);
+    }, [itemRef, props.setWidth]);
 
     const Items = useMemo(() => (
         props.items.map((item, i) => {
             let id = item.id;
             if (props.specialCase && (i==0 || i==props.items.length))
                 id += ("" + i);
-
+            
             return (
-                <SliderItem className="slider-item" key={id}>
+                <SliderItem className="slider-item" key={id} ref={i === 0 ? itemRef : undefined}>
                     {props.render(item)}
                 </SliderItem>
             );
-        })), [props.items, props.render, props.specialCase]);
+        })), [props.items, props.render, props.specialCase, itemRef]);
 
     return (
-        <SliderContainer>
+        <SliderContainer
+            ref={ref}
+        >
             {Items}
         </SliderContainer>
     );
-}
+});
 
 export default SliderItems;
