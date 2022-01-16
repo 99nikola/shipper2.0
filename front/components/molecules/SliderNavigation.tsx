99eead8c88f877bdabd081210d3bb023cad4e8e4
99nikola <nikola.popovicc99@gmail.com>
2022-01-16 16:32:25 +0100

    slider navigation (left/right)

---
+++ front/components/molecules/SliderNavigation.tsx
@@ -0,0 +1,28 @@
+import { ButtonGroup } from "@mui/material";
+import { memo } from "react";
+import Button from "../atoms/Button";
+import { ArrowLeft, ArrowRight } from "../styled/Arrows.syled";
+
+interface SliderNavigationProps {
+    slideLeft: () => void
+    slideRight: () => void
+}
+
+const SliderNavigation: React.FC<SliderNavigationProps> = (props) => {
+    return (
+        <ButtonGroup>
+            <Button
+                onClick={props.slideLeft}>
+                <ArrowLeft size="2.5rem" />
+            </Button>
+
+            <Button
+                onClick={props.slideRight}
+            >
+                <ArrowRight size="2.5rem" />
+            </Button>
+        </ButtonGroup>
+    )
+}
+
+export default memo(SliderNavigation);
