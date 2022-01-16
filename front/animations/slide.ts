82d61dd7f62db1459680d4a4902c9b1993a56a24
99nikola <nikola.popovicc99@gmail.com>
2022-01-16 16:31:54 +0100

    js animation added (requrestAnimationFrame)

---
+++ front/animations/slide.ts
@@ -0,0 +1,41 @@
+export enum EDir {
+    LEFT = "-",
+    RIGHT = "+"
+}
+
+let animationFrame: number | null = null;
+
+export function slide(element: HTMLElement, distance: number, dir = EDir.RIGHT, duration: number) {
+    return new Promise<void>(resolve => {
+        if (animationFrame !== null) 
+            return;
+
+        let start: DOMHighResTimeStamp, previousTimeStamp: DOMHighResTimeStamp;
+        let speed = distance / duration;
+    
+        const step = (timestamp: DOMHighResTimeStamp) => {
+            if (start === undefined) {
+                start = timestamp;
+            }
+            const elapsed = timestamp - start;
+    
+            if (previousTimeStamp !== timestamp) {
+                const count = Math.min(speed * elapsed, distance);
+                element.style.transform = "translateX(" + dir + count + "px)";
+            }
+    
+            if (elapsed < duration) {
+                previousTimeStamp = timestamp
+                animationFrame = window.requestAnimationFrame(step);
+            } else {
+                setTimeout(() => {
+                    window.requestAnimationFrame(() => element.style.transform = "translateX(0px)");
+                    animationFrame = null;
+                    resolve();
+                });
+            }
+        }
+    
+        animationFrame = window.requestAnimationFrame(step);
+    });
+}
